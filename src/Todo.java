3492eec9e7db536a748c6e4dbc70a97b043ebfeb
MickeyJ <develop.agility@gmail.com>
2016-06-13 12:44:45 -0600

    creating JSON objects

--- src/Todo.java
+++ src/Todo.java
@@ -1,34 +1,30 @@
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Todo {
+  JSONObject todo = new JSONObject();
   private int id;
   private String task;
   private boolean complete;
 
-  Todo(){
+  public Todo(){
     id = 0;
     task = "";
     complete = false;
-  }
-
-  public Object getTodoObj(){
-    return this;
   }
 
   public void setTodo(int id, String task){
     this.id = id;
     this.task = task;
     this.complete = false;
-
   }
 
-  public String getTodo(){
-    return (
-      "{" +
-      "\n  \"id\": \"" + this.id +"\"," +
-      "\n  \"task\": \"" + this.task +"\"," +
-      "\n  \"complete\": \"" + this.complete +"\"" +
-      "\n }"
-    );
+  public Object getTodo() throws JSONException {
+    todo.put( "id", this.id );
+    todo.put( "task", this.task );
+    todo.put( "complete", this.complete );
+
+    return todo;
   }
 
 }
--- src/TodoArray.java
+++ src/TodoArray.java
@@ -1,6 +1,9 @@
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,12 +12,11 @@ public class TodoArray {
   ArrayList list = new ArrayList();
   Scanner input = new Scanner(System.in);
   String todoFile = "assets/todos.json";
-  FileReader readFile;
-  FileWriter writeFile;
   Todo[] todos;
 
   public static void main(String[] args){
     TodoArray todoArray = new TodoArray();
+
     todoArray.initREPL();
   }
 
@@ -30,7 +32,7 @@ public class TodoArray {
     switch(input){
       case "add": pushMany();
         break;
-      case "list": listAll();
+      case "list": listTodos();
         break;
       case "empty": popAll();
         break;
@@ -43,7 +45,6 @@ public class TodoArray {
   }
 
   public void pushMany(){
-
     System.out.println("\nHow many new things to do?");
     todos = new Todo[input.nextInt()];
 
@@ -51,52 +52,62 @@ public class TodoArray {
       System.out.println("\nEnter a task");
       todos[i] = new Todo();
       todos[i].setTodo(i, input.next());
-      list.add(todos[i].getTodo());
+
+      try { list.add(todos[i].getTodo()); }
+      catch (JSONException e) { e.printStackTrace(); }
     }
 
-    System.out.print("\n\"todos\": ");
-    System.out.println(list);
-
-    try {
-      writeFile = new FileWriter(todoFile, false);
-      writeFile.write(String.valueOf(list));
-      writeFile.flush();
-      writeFile.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
+    writeFile(String.valueOf(list));
+    list.clear();
+    listTodos();
   }
 
   public void popAll(){
+    writeFile("[]");
+    System.out.println("\nTodo List is Now Empty");
+  }
+
+  public void listTodos() {
+    String jsonData = readFile(todoFile);
+    System.out.println();
 
     try {
-      writeFile = new FileWriter(todoFile);
-      writeFile.write("[]");
-      writeFile.close();
-      System.out.println("\nTodo List is Now Empty");
+      JSONArray jsonArr = new JSONArray(jsonData);
 
-    } catch (IOException e) {
+      for(int i = 0; i < jsonArr.length(); i++) {
+        int id = jsonArr.getJSONObject(i).getInt("id");
+        String task = jsonArr.getJSONObject(i).getString("task");
+        System.out.println(id +" | "+ task);
+      }
+
+    } catch (JSONException e) {
       e.printStackTrace();
     }
 
   }
 
-  public void listAll(){
-
-    System.out.println();
-
+  public static String readFile(String filename) {
+    String result = "";
     try {
-      readFile = new FileReader(todoFile);
-      int ch;
-      System.out.print("\"todos\": ");
-      do {
-        ch = readFile.read();
-        if (ch != -1) System.out.print((char) ch);
-      } while (ch != -1);
-      readFile.close();
-      System.out.println();
+      BufferedReader file = new BufferedReader(new FileReader(filename));
+      StringBuilder string = new StringBuilder();
+      String line = file.readLine();
+      while (line != null) {
+        string.append(line);
+        line = file.readLine();
+      }
+      result = string.toString();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    return result;
+  }
+
+  public void writeFile(String input){
+    try {
+      FileWriter writeFile = new FileWriter(todoFile, false);
+      writeFile.write(input);
+      writeFile.close();
 
     } catch (IOException e) {
       e.printStackTrace();
